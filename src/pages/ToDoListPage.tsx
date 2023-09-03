21289dcdb68ebf66e9e8893a1576647b977b1726
Kseniya Yaskevich <yaskevi4kseniya@gmail.com>
2023-09-03 15:20:46 +0300

    add react-toastify

--- src/pages/ToDoListPage.tsx
+++ src/pages/ToDoListPage.tsx
@@ -3,9 +3,15 @@ import { Form } from "../components/Form/Form"
 import { Header } from "../components/Header/Header"
 import { ToDoList } from "../components/ToDoList/ToDoList"
 import { ToDo } from "../models/todo-item"
+import { toast, ToastContainer } from "react-toastify"
+import 'react-toastify/dist/ReactToastify.css';
 
 export const ToDoListPage = () => {
     const [todos, setTodos] = useState<ToDo[]>([])
+
+    const notify = (text: string) => {
+        toast(text)
+    }
 
     const createNewToDo = (text: string) => {
         const newToDo: ToDo = {
@@ -14,12 +20,19 @@ export const ToDoListPage = () => {
             isDone: false 
         }
         setTodos([...todos, newToDo])
+        notify('Добавлена новая задача')
     }
 
     const updateToDo = (toDoItem: ToDo) => {
         const newTodos = todos.map((item) => {
             if(item.id === toDoItem.id){
                 item.isDone = !item.isDone
+
+                if(item.isDone){
+                    notify('Задача выполнена')
+                } else {
+                    notify('Задача НЕ выполнена')
+                }
             }
             return item
         })
@@ -29,13 +42,26 @@ export const ToDoListPage = () => {
     const deleteToDo = (toDoItem: ToDo) => {
         const newTodos = todos.filter((item) => item.id !== toDoItem.id)
         setTodos(newTodos)
+        notify('Задача удалена')
     }
 
     return (
         <>
             <Header />
-            <Form createNewToDo={createNewToDo}/>
-            <ToDoList todos={todos} updateToDo={updateToDo} deleteToDo={deleteToDo}/>
+            <Form createNewToDo={createNewToDo} />
+            <ToDoList todos={todos} updateToDo={updateToDo} deleteToDo={deleteToDo} />
+            <ToastContainer 
+               position="bottom-right"
+               autoClose={5000}
+               hideProgressBar={false}
+               newestOnTop={false}
+               closeOnClick
+               rtl={false}
+               pauseOnFocusLoss
+               draggable
+               pauseOnHover
+               theme="dark" 
+            />
         </>
     )
 }
