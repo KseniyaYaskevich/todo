d89db79100420b6d6225f233782dba7db053d578
Kseniya Yaskevich <yaskevi4kseniya@gmail.com>
2023-09-04 15:48:44 +0300

    complete lesson 4

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,69 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './assets/scss/normalize.scss';
 import './assets/scss/style.scss';
+import { Layout } from './layouts/Layout';
+import { ToDo } from './models/todo-item';
+import { NotFound } from './pages/404';
+import { HomePage } from './pages/HomePage';
+import { ItemDescription } from './pages/ItemDescription';
 import { ToDoListPage } from './pages/ToDoListPage';
+
+const todos: ToDo[] = [
+  {
+    id: 0,
+    text: 'Первое действие',
+    isDone: false
+  },
+  {
+    id: 1,
+    text: '2 действие',
+    isDone: true
+  },
+  {
+    id: 2,
+    text: '3 действие',
+    isDone: false
+  },
+  {
+    id: 3,
+    text: '4 действие',
+    isDone: true
+  }
+]
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage todos={todos} />
+      },
+      {
+        path: '/todo',
+        element: <ToDoListPage />
+      },
+      {
+        path: '/list/:id',
+        element: <ItemDescription todos={todos} />
+      },
+    ]
+  },
+  {
+    path: '*',
+    element: <NotFound />
+  }
+], {basename: '/app/'})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ToDoListPage/>
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
